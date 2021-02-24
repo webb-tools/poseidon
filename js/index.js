6ef94fe9b082067d3b67991f880f413beeb0fda9
Shady Khalifa <shekohex@gmail.com>
2021-02-24 18:32:16 +0000

    support preloading gens to the hasher

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+// @ts-check
 import("../pkg/index.js")
   .then(pkg => {
-    console.time('Creating PoseidonHasher');
-    pkg.PoseidonHasher.new();
-    console.timeEnd('Creating PoseidonHasher');
+    console.time('Creating PoseidonHasher without cached gens');
+    const opts1 = new pkg.PoseidonHasherOptions();
+    new pkg.PoseidonHasher(opts1);
+    console.timeEnd('Creating PoseidonHasher without cached gens');
+
+    const opts2 = new pkg.PoseidonHasherOptions();
+    // we pay the time for ser/de the values tho!
+    const gens = opts2.bp_gens; // regenerate the gens (if not already there).
+    opts2.bp_gens = gens;
+    console.time('Creating PoseidonHasher with cached gens');
+    new pkg.PoseidonHasher(opts2);
+    console.timeEnd('Creating PoseidonHasher with cached gens');
   })
   .catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use bulletproofs::BulletproofGens;
+use bulletproofs::{BulletproofGens, PedersenGens};
 use curve25519_gadgets::poseidon::{
     builder::{Poseidon, PoseidonBuilder},
     gen_mds_matrix, gen_round_keys, PoseidonSbox,
 };
+use js_sys::Uint8Array;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -21,26 +22,90 @@ pub fn set_panic_hook() {
 }
 
 #[wasm_bindgen]
+pub struct PoseidonHasherOptions {
+    /// The size of the permutation, in field elements.
+    width: usize,
+    /// Number of full SBox rounds in beginning
+    pub full_rounds_beginning: Option<usize>,
+    /// Number of full SBox rounds in end
+    pub full_rounds_end: Option<usize>,
+    /// Number of partial rounds
+    pub partial_rounds: Option<usize>,
+    /// The desired (classical) security level, in bits.
+    pub security_bits: Option<usize>,
+    /// Bulletproof generators for proving/verifying (serialized)
+    #[wasm_bindgen(skip)]
+    pub bp_gens: Option<BulletproofGens>,
+}
+
+impl Default for PoseidonHasherOptions {
+    fn default() -> Self {
+        Self {
+            width: 6,
+            full_rounds_beginning: None,
+            full_rounds_end: None,
+            partial_rounds: None,
+            security_bits: None,
+            bp_gens: None,
+        }
+    }
+}
+
+#[wasm_bindgen]
+impl PoseidonHasherOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self { Self::default() }
+
+    #[wasm_bindgen(setter)]
+    pub fn set_bp_gens(&mut self, value: Uint8Array) {
+        let bp_gens: BulletproofGens = bincode::deserialize(&value.to_vec())
+            .unwrap_or_else(|_| BulletproofGens::new(4096, 1));
+        self.bp_gens = Some(bp_gens);
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn bp_gens(&self) -> Uint8Array {
+        let val = self
+            .bp_gens
+            .clone()
+            .unwrap_or_else(|| BulletproofGens::new(4096, 1));
+        let serialized =
+            bincode::serialize(&val).unwrap_or_else(|_| Vec::new());
+        Uint8Array::from(serialized.as_slice())
+    }
+}
+
+#[wasm_bindgen]
 pub struct PoseidonHasher {
     inner: Poseidon,
 }
 
-impl Default for PoseidonHasher {
-    fn default() -> Self { Self::new() }
-}
-
 #[wasm_bindgen]
 impl PoseidonHasher {
-    pub fn new() -> Self {
-        let width = 6;
-        let (full_b, full_e) = (4, 4);
-        let partial_rounds = 57;
-        let inner = PoseidonBuilder::new(width)
-            .num_rounds(full_b, full_e, partial_rounds)
-            .round_keys(gen_round_keys(width, full_b + full_e + partial_rounds))
-            .mds_matrix(gen_mds_matrix(width))
+    pub fn default() -> Self { Self::with_options(Default::default()) }
+
+    #[wasm_bindgen(constructor)]
+    pub fn with_options(opts: PoseidonHasherOptions) -> Self {
+        let full_rounds_beginning = opts.full_rounds_beginning.unwrap_or(3);
+        let full_rounds_end = opts.full_rounds_end.unwrap_or(3);
+        let partial_rounds = opts.partial_rounds.unwrap_or(57);
+
+        // default pedersen genrators
+        let pc_gens = PedersenGens::default();
+        let bp_gens = opts
+            .bp_gens
+            .clone()
+            .unwrap_or_else(|| BulletproofGens::new(4096, 1));
+
+        let total_rounds =
+            full_rounds_beginning + full_rounds_end + partial_rounds;
+        let inner = PoseidonBuilder::new(opts.width)
+            .num_rounds(full_rounds_beginning, full_rounds_end, partial_rounds)
+            .round_keys(gen_round_keys(opts.width, total_rounds))
+            .mds_matrix(gen_mds_matrix(opts.width))
             .sbox(PoseidonSbox::Inverse)
-            .bulletproof_gens(BulletproofGens::new(4096, 1))
+            .bulletproof_gens(bp_gens)
+            .pedersen_gens(pc_gens)
             .build();
         Self { inner }
     }
